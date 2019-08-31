d94df3c5231bc5b089358857b7a7d2904dfce045
banxi1988 <banxi1988@gmail.com>
2019-08-31 17:31:25 +0800

    add: 添加基本的元素组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
-function currentTime() {
-  return new Date().toISOString();
-}
+import Game from "./Game";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>当前时间: {currentTime()} </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game />
     </div>
   );
 };
---
+++ src/Game.tsx
@@ -0,0 +1,53 @@
+import { Component } from "react";
+import React from "react";
+import "./Game.css";
+
+class Square extends Component {
+  render() {
+    return <button className="square">{/*TODO*/}</button>;
+  }
+}
+
+class Board extends Component {
+  renderSquare(i: number) {
+    return <Square />;
+  }
+  render() {
+    const status = "下一个玩家 : X";
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/*status */}</div>
+        </div>
+      </div>
+    );
+  }
+}
