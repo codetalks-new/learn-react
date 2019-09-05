67e0d3996d391b4cacdb40d7ee9925e82db4bb8b
banxi1988 <banxi1988@gmail.com>
2019-09-05 15:45:41 +0800

    add: 将 MouseTracker 使用 hook 改成 FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component, MouseEvent, createRef } from "react";
+import React, { Component, MouseEvent, createRef, useState, useEffect } from "react";
 import "./App.css";
 import imgCat from "./cat.jpeg";
 
@@ -46,9 +46,9 @@ class Cat extends Component<{ mouse: Point }> {
     let top = y;
     if (img) {
       const { clientWidth, clientHeight } = img;
-      console.info(
-        `img(clientWidth,clientHeight):${img.clientWidth}, ${img.clientHeight},img(width,height):${img.width},${img.height}`
-      );
+      // console.info(
+      // `img(clientWidth,clientHeight):${img.clientWidth}, ${img.clientHeight},img(width,height):${img.width},${img.height}`
+      // );
       left = x - clientWidth * 0.5;
       top = y - clientHeight * 0.5;
     }
@@ -57,21 +57,30 @@ class Cat extends Component<{ mouse: Point }> {
   }
 }
 
-class MouseTrakcer extends Component {
-  render() {
-    return (
-      <div style={{ height: "100%" }}>
-        <h3>在旁边移到下鼠标吧!</h3>
-        <Mouse render={mouse => <Cat mouse={mouse} />} />
-      </div>
-    );
-  }
-}
+const MouseTracker = () => {
+  const [mouse, setMouse] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event: MouseEvent) => {
+    setMouse({
+      x: event.clientX,
+      y: event.pageY
+    });
+    // console.info(
+    // `client(X,Y):${event.clientX},${event.clientY}, page(X,Y):${event.pageX},${event.pageY}, screen(X,Y):${event.screenX},${event.screenY},movement(X,Y):${event.movementX},${event.movementY}`
+    // );
+  };
+  return (
+    <div style={{ height: "100%" }} onMouseMove={handleMouseMove}>
+      <h3>在旁边移动下鼠标吧!</h3>
+      <Cat mouse={mouse} />
+    </div>
+  );
+};
 
 const App: React.FC = () => {
   return (
     <div className="App" style={{ height: "100%" }}>
-      <MouseTrakcer />
+      <MouseTracker />
     </div>
   );
 };
