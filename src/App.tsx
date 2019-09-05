c1e8da73a325ae330af186e07df689fd67516f00
banxi1988 <banxi1988@gmail.com>
2019-09-05 14:04:24 +0800

    add: step2  将 Mouse 从 MouseTracker 作为单独的组件提取出来

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Component, MouseEvent } from "react";
 import "./App.css";
 
-class MouseTrakcer extends Component {
+class Mouse extends Component {
   state = {
     x: 0,
     y: 0
@@ -12,15 +12,24 @@ class MouseTrakcer extends Component {
       y: event.clientY
     });
   };
-
   render() {
     const { x, y } = this.state;
     return (
       <div style={{ height: "100%" }} onMouseMove={this.handleMouseMove}>
-        <h3>在旁边移动下鼠标吧!</h3>
         <p>
           鼠标当前坐标: ({x},{y})
         </p>
+      </div>
+    );
+  }
+}
+
+class MouseTrakcer extends Component {
+  render() {
+    return (
+      <div>
+        <h3>在旁边移到下鼠标吧!</h3>
+        <Mouse />
       </div>
     );
   }
