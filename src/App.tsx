f4a0c6c078de61ce85dced3bc4edf974fb643edf
banxi1988 <banxi1988@gmail.com>
2019-09-05 14:43:46 +0800

    add: 引入增加 Cat 组件让小猫咪跟着鼠标移动

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React, { Component, MouseEvent } from "react";
+import React, { Component, MouseEvent, createRef } from "react";
 import "./App.css";
+import imgCat from "./cat.jpeg";
 
-class Mouse extends Component {
+interface Point {
+  x: number;
+  y: number;
+}
+
+class Mouse extends Component<{ render: (mouse: Point) => any }> {
   state = {
     x: 0,
     y: 0
@@ -13,14 +19,32 @@ class Mouse extends Component {
     });
   };
   render() {
-    const { x, y } = this.state;
     return (
       <div style={{ height: "100%" }} onMouseMove={this.handleMouseMove}>
-        <p>
-          鼠标当前坐标: ({x},{y})
-        </p>
+        <p>{this.props.render(this.state)}</p>
       </div>
     );
+  }
+}
+
+class Cat extends Component<{ mouse: Point }> {
+  imgRef = createRef<HTMLImageElement>();
+  componentDidMount() {
+    console.info("Cat componentDidMount");
+  }
+  render() {
+    console.info("Cat render");
+    const { x, y } = this.props.mouse;
+    const img = this.imgRef.current;
+    let left = x;
+    let right = y;
+    if (img) {
+      const { clientWidth, clientHeight } = img;
+      left = x - clientWidth * 0.5;
+      right = y - clientHeight * 0.5;
+    }
+
+    return <img ref={this.imgRef} src={imgCat} alt="小猫" style={{ position: "absolute", left: left, right: right }} />;
   }
 }
 
@@ -29,7 +53,7 @@ class MouseTrakcer extends Component {
     return (
       <div>
         <h3>在旁边移到下鼠标吧!</h3>
-        <Mouse />
+        <Mouse render={mouse => <Cat mouse={mouse} />} />
       </div>
     );
   }
