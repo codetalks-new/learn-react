94e8f6fd28a3a13d9c56668c4150c775de1b1fb0
banxi1988 <banxi1988@gmail.com>
2019-09-12 22:41:58 +0800

    add: 添加简单的 Context 使用示例

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,48 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function currentTime() {
-  return new Date().toISOString();
+const ThemeContext = React.createContext("light");
+const LangContext = React.createContext("中文");
+const Button = () => (
+  <ThemeContext.Consumer>
+    {theme => (
+      <LangContext.Consumer>
+        {lang => (
+          <button>
+            主题色为:{theme}, 当前语言为: {lang}
+          </button>
+        )}
+      </LangContext.Consumer>
+    )}
+  </ThemeContext.Consumer>
+);
+
+const ThemedButton = () => <Button />;
+
+class MyText extends Component {
+  static contextType = ThemeContext;
+  render() {
+    const theme = this.context;
+    console.warn("current theme:", theme);
+    return <span>当前主题色:{theme}</span>;
+  }
 }
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>当前时间: {currentTime()} </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
+const Toolbar = () => (
+  <div>
+    <ThemedButton />
+    <MyText />
+  </div>
+);
+
+const App: React.FC = () => (
+  <div className="App">
+    <ThemeContext.Provider value="dark">
+      <LangContext.Provider value="中文">
+        <Toolbar />
+      </LangContext.Provider>
+    </ThemeContext.Provider>
+  </div>
+);
 
 export default App;
